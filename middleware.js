ab65eea9e53a248824125a9d982fa3a1703ddec3
saichavan19 <saichavan19.sc@gmail.com>
2025-05-10 19:22:05 +0530

    Add files via upload

---
+++ middleware.js
@@ -0,0 +1,64 @@
+const Listing = require("./models/listingm.js");
+const Review = require("./models/reviewm.js");
+const ExpressError = require("./utils/ExpressError.js");
+const {listingSchema} = require("./schema.js");
+const {reviewSchema} = require("./schema.js");
+
+module.exports.isLoggedIn = (req, res, next) => {
+    if(!req.isAuthenticated()) {
+    req.session.redirectUrl = req.originalUrl;
+    req.flash("error","You must be login in to create listing");
+    return res.redirect("/login");
+    }
+    next();
+};
+
+module.exports.saveRedirectUrl = (req, res, next) => {
+    if(req.session.redirectUrl) {
+        res.locals.redirectUrl = req.session.redirectUrl;
+    }
+    next();
+};
+
+module.exports.isOwner = async(req, res, next) => {
+    let {id} = req.params;
+    let listing = await Listing.findById(id);
+    if(!listing.owner._id.equals(res.locals.currentUsr._id)) {
+        req.flash("error","You are not a owner of this listing");
+        return res.redirect(`/listings/${id}`);
+    }
+    next();
+};
+
+module.exports.validateListing = (req, res, next) => {
+    let {error} = listingSchema.validate(req.body);
+       
+        if(error) {
+            let errMsg = error.details.map((el) => el.message).join(",");
+            throw new ExpressError(400, errMsg);
+        }else{
+            next();
+        }
+    }; 
+
+module.exports.validateReview = (req, res, next) => {
+    let {error} = reviewSchema.validate(req.body);
+
+    if(error){
+        let errMsg = error.details.map((el) => el.message).join(",");
+        throw new ExpressError(400, errMsg);
+    }else{
+        next();
+    }
+};
+
+module.exports.isReviewAuthor = async(req, res, next) => {
+    let {id, reviewId} = req.params;
+    let review = await Review.findById(reviewId);
+    if(!review.author.equals(res.locals.currentUsr._id)) {
+        req.flash("error","It's not your");
+        return res.redirect(`/listings/${id}`);
+    }
+    next();
+};
+
